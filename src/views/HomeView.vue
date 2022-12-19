179ef09d10ed3d59475308071bd8fd63bbd74075
eprikhodko <eprikhodkodev@gmail.com>
2022-12-19 14:46:29 +0400

    clean up project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main><h1>This is home page</h1></main>
 </template>
