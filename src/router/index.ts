460776eb61c5eae1a124df8607f1d46d3dac6148
eprikhodko <eprikhodkodev@gmail.com>
2022-12-19 16:21:37 +0400

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import PageNotFoundView from "../views/PageNotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AddProductView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "page-not-found",
+      component: PageNotFoundView,
     },
   ],
 });
